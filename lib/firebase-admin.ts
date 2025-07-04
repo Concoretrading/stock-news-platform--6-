4a74bb84d936baf1e5c00e665af24482d66d7360
Concoretrading <handrigannick@gmail.com>
2025-07-04 11:56:50 -0400

    Use GOOGLE_APPLICATION_CREDENTIALS_JSON for Firebase Admin credentials if set

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,7 +4,9 @@ import { getStorage } from 'firebase-admin/storage';
 
 if (!getApps().length) {
   initializeApp({
-    credential: applicationDefault(),
+    credential: process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+      ? cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON))
+      : applicationDefault(),
   });
 }
 
